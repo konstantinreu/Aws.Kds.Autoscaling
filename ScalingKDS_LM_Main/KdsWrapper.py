7335b5cd6ac78849d2a1fb1aba66c71337c2ea59
Kostiantyn <38301530+konstantinreu@users.noreply.github.com>
2019-04-23 00:02:32 -0700

    Update KdsWrapper.py

--- ScalingKDS_LM_Main/KdsWrapper.py
+++ ScalingKDS_LM_Main/KdsWrapper.py
@@ -167,26 +167,3 @@ def waitActiveState4KDS(KdsName):
         if intervalWaited > (timeoutSec / sleepIntervalSec):
             raise Exception('Timeout for waiting ACTIVE status of KDS:' + KdsName)
 
-
-
-'''
-
-Summary: {"StreamDescriptionSummary": {"StreamName": "rdm-dev-intake-kds-main",
-                                       "StreamARN": "arn:aws:kinesis:us-west-2:236573294224:stream/rdm-dev-intake-kds-main",
-                                       "StreamStatus": "ACTIVE", "RetentionPeriodHours": 24,
-                                       "StreamCreationTimestamp": "2018-05-02T10:29:35-07:00",
-                                       "EnhancedMonitoring": [{"ShardLevelMetrics": ["IncomingBytes",
-                                                                                     "OutgoingRecords",
-                                                                                     "IteratorAgeMilliseconds",
-                                                                                     "IncomingRecords",
-                                                                                     "ReadProvisionedThroughputExceeded",
-                                                                                     "WriteProvisionedThroughputExceeded",
-                                                                                     "OutgoingBytes"]}],
-                                       "EncryptionType": "NONE", "OpenShardCount": 5, "ConsumerCount": 0},
-          "ResponseMetadata": {"RequestId": "c97927d3-da18-403f-9df3-2749dd1328a8", "HTTPStatusCode": 200,
-                               "HTTPHeaders": {"x-amzn-requestid": "c97927d3-da18-403f-9df3-2749dd1328a8",
-                                               "x-amz-id-2": "cwe0EixBNYyfRyC05HGKL0sVs1PGIfOvaDCiOqt1;gr5PKxBWHz2+sY3O05SoRPocvK/bx6gqeVkrAZFn05ujaj0NRPrAUm",
-                                               "date": "Tue, 11 Dec 2018 18:13:46 GMT",
-                                               "content-type": "application/x-amz-json-1.1",
-                                               "content-length": "518"}, "RetryAttempts": 0}}
-'''
